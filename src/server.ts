42e3529addc76fd9b2e0d5091ef01641a23ce57c
Arthur Pedroti <arthurpedroti@gmail.com>
2023-04-19 13:53:06 -0300

    connection database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const tables = await knex('SALDO_ESTOQUE').select('*')
+
+  return tables
 })
 
 app
